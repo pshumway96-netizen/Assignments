5b1c6a0cf3a04e08a03bcc958b6178bcfce749c4
Parker Shumway <pshumway96@gmail.com>
2026-06-28 19:52:59 -0700

    Create first_assembly.asm

---
+++ first_assembly.asm
@@ -0,0 +1,19 @@
+Code:
+
+section .data
+    msg: db "I came,", 10, "I saw,", 10, "I conquered.", 10
+    msg_len: equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, msg_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
