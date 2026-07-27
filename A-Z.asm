3cb520002485dfd0759fd5b3e7e412d994bc87ac
Parker Shumway <pshumway96@gmail.com>
2026-07-26 21:11:24 -0700

    Update A-Z.asm

--- A-Z.asm
+++ A-Z.asm
@@ -1,1 +1,40 @@
+section .data
+    current_char db 'A'
+    newline      db 10
 
+section .text
+    global _start
+
+_start:
+    mov ecx, 26
+
+print_loop:
+    push ecx
+
+    call print_char
+    call print_newline
+
+    inc byte [current_char]
+
+    pop ecx
+    loop print_loop
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+print_char:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, current_char
+    mov edx, 1
+    int 0x80
+    ret
+
+print_newline:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+    ret
